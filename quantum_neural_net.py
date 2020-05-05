539db74937364e3911f95ef41bbb8be6fed62238
Rafael Haenel <rafaelhaenel@phas.ubc.ca>
2020-05-05 15:12:56 -0700

    grover notebook added

--- quantum_neural_net.py
+++ quantum_neural_net.py
@@ -65,23 +65,36 @@ plt.show()
 
 np.random.seed(0)
 num_layers = 6
-var_init = 0.1 * np.random.randn(num_layers, 5)
-print(var_init)
+# var_init = 0.1 * np.random.randn(num_layers, 5)
+# print(var_init)
+
+var_init= np.array([[ 0.1672444 , -0.02746   ,  0.08835575,  0.18722878,  0.18929305],
+                    [-0.0957819 ,  0.12777733, -0.01524759, -0.05143529,  0.05080969],
+                    [ 0.01374947,  0.18976937,  0.08813493, -0.02532584,  0.07449287],
+                    [ 0.04830769,  0.16256158,  0.02936889, -0.00605395, -0.05571956],
+                    [-0.20538369,  0.10427885,  0.12307134, -0.11345193,  0.24573954],
+                    [-0.10939146, -0.01677891,  0.02856047,  0.1096455 ,  0.16252224]])
 
 opt = AdamOptimizer(0.01, beta1=0.9, beta2=0.999)
 
 var = var_init
-for it in range(500):
+for it in range(10):
     var = opt.step(lambda v: cost(v, X, Y), var)
     print("Iter: {:5d} | Cost: {:0.7f} ".format(it + 1, cost(var, X, Y)))
 
 
-x_pred = np.linspace(np.min(X), np.max(X)*2, 100)
-predictions = [quantum_neural_net(var, x=x_) for x_ in x_pred]
+var= np.array([[ 0.1672444 , -0.02746   ,  0.08835575,  0.18722878,  0.18929305],
+                    [-0.0957819 ,  0.12777733, -0.01524759, -0.05143529,  0.05080969],
+                    [ 0.01374947,  0.18976937,  0.08813493, -0.02532584,  0.07449287],
+                    [ 0.04830769,  0.16256158,  0.02936889, -0.00605395, -0.05571956],
+                    [-0.20538369,  0.10427885,  0.12307134, -0.11345193,  0.24573954],
+                    [-0.10939146, -0.01677891,  0.02856047,  0.1096455 ,  0.16252224]])
+x_pred = np.linspace(np.min(X), np.max(X)*5, 100)
+predictions = np.array([quantum_neural_net(var, x=x_) for x_ in x_pred])
 
 plt.figure()
-plt.scatter(X, Y)
-plt.scatter(x_pred, predictions, color="green")
+plt.scatter(X*xmax, Y*ymax)
+plt.plot(x_pred*xmax, predictions*ymax, color="green")
 plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.tick_params(axis="both", which="major")
